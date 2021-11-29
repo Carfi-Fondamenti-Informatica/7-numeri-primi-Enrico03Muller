eced8d56960b17610156430051bee109257964bb
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-11-29 16:31:59 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,25 @@
+//
+// Created by Enrico Muller on 29/11/2021.
+//
 
+
+/*int funzione (int n){
+    for (int i = 2; i <= n; i++){
+        int temp = n % i;
+        if (temp == 0 && i != n){
+            return 0;
+        }
+    }
+    return 1;
+}*/
+
+int test (int n){
+    int i = 2;
+    int temp = n % i;
+    if (temp == 0 && i != n){
+        return 0;
+    }else{
+        test(n-1);
+    }
+    return 1;
+}
