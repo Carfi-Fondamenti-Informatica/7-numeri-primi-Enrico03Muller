c3833a69d0abdf30ac58e775ff46b74f6d8ee0d3
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-11-29 17:21:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int n;
     int i = 2;
-    //cout << "Inserire un numero: " << endl;
+    cout << "Inserire un numero: " << endl;
     cin >> n;
     if (n <= 1){
         cout << "numero non primo";
