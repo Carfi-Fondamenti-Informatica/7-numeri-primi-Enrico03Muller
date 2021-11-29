59bc92ab729ba57b56f24a182bcb031425aca732
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-11-29 16:38:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,14 @@ int main() {
     int n;
     cout << "Inserire un numero: " << endl;
     cin >> n;
-    if (test(n) == 1){
-        cout << "numero primo";
-    }else{
+    if (n <= 1){
         cout << "numero non primo";
+    }else {
+        if (test(n) == 1) {
+            cout << "numero primo";
+        } else {
+            cout << "numero non primo";
+        }
     }
     return 0;
 }
