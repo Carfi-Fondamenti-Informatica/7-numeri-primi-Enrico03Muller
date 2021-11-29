d203561edd56efe0e695e694c3992ebbc6327f7b
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-11-29 16:31:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int n;
+    cout << "Inserire un numero: " << endl;
+    cin >> n;
+    if (test(n) == 1){
+        cout << "numero primo";
+    }else{
+        cout << "numero non primo";
+    }
+    return 0;
 }
